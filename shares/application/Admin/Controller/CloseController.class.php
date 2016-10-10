cec82135a5deb0ae8d75419b341a7f36928366bc
asstitle <794863612@qq.com>
2016-10-10 14:42:14 +0800

    v

--- shares/application/Admin/Controller/CloseController.class.php
+++ shares/application/Admin/Controller/CloseController.class.php
@@ -13,8 +13,38 @@ use Common\Controller\AdminbaseController;
 
 class CloseController extends AdminbaseController
 {
-     public function index(){
+    function _initialize() {
+        parent::_initialize();
+        $this->table = M("warehouse");
+    }
+    public function index(){
 
-         $this->display();
-      }
+        $fields = array(
+            'start_time'=> array("field"=>"w.flat_time","operator"=>">"),
+            'end_time'  => array("field"=>"w.flat_time","operator"=>"<"),
+            'keyword'  => array("field"=>"ou.user_nicename","operator"=>"like"),
+            'keyword1'  => array("field"=>"ou.mobile","operator"=>"like"),
+            'keyword2'  => array("field"=>"w.goods_name","operator"=>"like"),
+        );
+
+        $where = $this->search($fields);
+        $count = $this->table
+            ->alias("w")
+            ->join(C ( 'DB_PREFIX' )."oauth_user ou ON w.user_id = ou.id")
+            ->where($where)
+            ->count();//获取条数
+        $page = $this->page($count, 20);//设置分页信息
+        $list = $this->table
+            ->alias("w")
+            ->join(C ( 'DB_PREFIX' )."oauth_user ou ON w.user_id = ou.id")
+            ->where($where)
+            ->limit($page->firstRow . ',' . $page->listRows)
+            ->field("w.*,ou.user_nicename,ou.mobile")//查询内容
+            ->order("id DESC")
+            ->select();
+
+        $this->assign("Page", $page->show('Admin'));
+        $this->assign("list",$list);
+        $this->display();
+    }
 }
